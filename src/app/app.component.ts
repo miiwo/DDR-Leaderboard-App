fc6312c9fec98f12ecc623529f1720e7980d8163
koipoi <koipond575@gmail.com>
2022-10-15 10:22:22 -0400

    Add loading bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,20 @@ export class AppComponent {
   leaderboard : LeaderboardItem[] = []
   page = 1
   buttonPressed = false
+  isLoading = false
 
   constructor(private googleSheets: GoogleDriveProvider) { }
 
   async fetchData(ddrEvent : string) {
     let data : LeaderboardItem[] = []
 
+    this.isLoading = true
     // fetch from data source and set leaderboard to data retrieved
     this.googleSheets.getSheetData(ddrEvent).subscribe(res => {
       console.log(res); 
       this.leaderboard = res.sort((x : LeaderboardItem, y : LeaderboardItem) => y.score - x.score)
       this.buttonPressed = true
+      this.isLoading = false
     })
 
     this.leaderboard = data
