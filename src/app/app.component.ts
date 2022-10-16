c1e031598b1bb98fdce5ac0b25c1650a87c8796f
koipoi <koipond575@gmail.com>
2022-10-16 09:54:02 -0400

    Add error handling on bad fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,14 @@ export class AppComponent {
   page = 1
   buttonPressed = false
   isLoading = false
+  errorFound = false
 
   constructor(private googleSheets: GoogleDriveProvider) { }
 
   async fetchData(ddrEvent : string) {
     let data : LeaderboardItem[] = []
 
+    this.errorFound = false
     this.isLoading = true
     // fetch from data source and set leaderboard to data retrieved
     this.googleSheets.getSheetData(ddrEvent).subscribe(res => {
@@ -26,6 +28,18 @@ export class AppComponent {
       this.leaderboard = res.sort((x : LeaderboardItem, y : LeaderboardItem) => y.score - x.score)
       this.buttonPressed = true
       this.isLoading = false
+    },
+    err => {
+      if (err.error instanceof Error) {
+        //A client-side or network error occurred.
+        console.log('An error occurred:', err.error.message);
+      } else {
+          //Backend returns unsuccessful response codes such as 404, 500 etc.
+          console.log('Backend returned status code: ', err.status);
+          console.log('Response body:', err.error);
+      }
+
+      this.errorFound = true
     })
 
     this.leaderboard = data
